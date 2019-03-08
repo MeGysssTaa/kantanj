8904689812859f17f7f4d602b0f80589f89b8425
DarksideCode <darksidecode@ya.ru>
2019-03-08 14:04:10 +0200

    Create ConcurrentObject<T>
    A handy class which may be used to simplify one's work with thread-safe operations on objects.

---
+++ src/main/java/me/darksidecode/kantanj/types/ConcurrentObject.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2019 DarksideCode
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.darksidecode.kantanj.types;
+
+import lombok.Getter;
+
+public class ConcurrentObject<T> {
+
+    @Getter
+    private final Object lock;
+
+    private T value;
+
+    public ConcurrentObject() {
+        this(new Object(), null);
+    }
+
+    public ConcurrentObject(T initialValue) {
+        this(new Object(), initialValue);
+    }
+
+    public ConcurrentObject(Object lock, T initialValue) {
+        this.lock = Check.notNull(lock, "lock cannot be null");
+        this.value = initialValue;
+    }
+
+    public void set(T newValue) {
+        synchronized (lock) {
+            value = newValue;
+        }
+    }
+
+    public T get() {
+        synchronized (lock) {
+            return value;
+        }
+    }
+
+    public T getAndSwap(T newValue) {
+        synchronized (lock) {
+            T oldValue = value;
+            value = newValue;
+
+            return oldValue;
+        }
+    }
+
+}
